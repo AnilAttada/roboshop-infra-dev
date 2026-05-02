c3690ecb1259039a3818786a9c53368907d69fae
AnilAttada <anilattada222@gmail.com>
2026-05-02 14:28:23 +0530

    ROBOSHOP INFRA ADDED

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -51,3 +51,9 @@ resource "aws_ssm_parameter" "catalogue_sg_id" {
   type = "String"
   value = module.catalogue.sg_id
 }
+
+resource "aws_ssm_parameter" "user_sg_id" {
+  name = "/${var.project}/${var.environment}/user_sg_id"
+  type = "String"
+  value = module.user.sg_id
+}
