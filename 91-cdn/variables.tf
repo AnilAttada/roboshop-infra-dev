1e83f17716ed98c88422878d35bf0ed48aa5b932
AnilAttada <anilattada222@gmail.com>
2026-05-15 06:55:30 +0530

    CDN ADDED

---
+++ 91-cdn/variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+variable "zone_id" {
+  default = "Z05167631MX6D5K0KPFAK"
+}
+variable "zone_name" {
+  default = "anilkumar.shop"
+}
